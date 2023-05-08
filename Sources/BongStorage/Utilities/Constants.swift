860822f4364e5b890af28225315361891d9b79db
이상봉 <bongbong9708@douzone.com>
2023-05-08 21:30:43 +0900

    1차 테스트 커밋

---
+++ Sources/BongStorage/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by 이상봉 on 2023/05/08.
+//
+
+import Foundation
+
+open class Constants {
+    public static let test = "이 글씨가 뜨면 성공!"
+}
---
+++ Sources/BongStorage/Utilities/Util.swift
@@ -0,0 +1,8 @@
+//
+//  Util.swift
+//  
+//
+//  Created by 이상봉 on 2023/05/08.
+//
+
+import Foundation
