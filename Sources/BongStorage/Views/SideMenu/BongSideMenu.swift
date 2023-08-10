17f3d1b1c529378ccf0b74f54d68c245891b8f14
이상봉 <bongbong9708@gmail.com>
2023-08-10 17:24:52 +0900

    2023. 08. 10 커스텀 사이드메뉴 구현

---
+++ Sources/BongStorage/Views/SideMenu/BongSideMenu.swift
@@ -0,0 +1,72 @@
+//
+//  BongSideMenu.swift
+//  
+//
+//  Created by 이상봉 on 2023/08/10.
+//
+
+import SwiftUI
+
+public struct BongSideMenu<Content>: View where Content: View {
+    
+    @Binding public var isPresented: Bool
+    public var width: CGFloat
+    public var content: Content
+    
+    @GestureState private var translation: CGFloat = .zero
+    
+    @Environment(\.safeAreaInsets) private var safeAreaInsets
+    
+    public init(_ isPresented: Binding<Bool>, width: CGFloat, content: () -> Content) {
+        self._isPresented = isPresented
+        self.width = width
+        self.content = content()
+    }
+    
+    public var body: some View {
+        ZStack {
+            Color.black.opacity(0.3)
+                .opacity(isPresented ? 1 : 0)
+                .onTapGesture {
+                    isPresented = false
+                }
+            
+            HStack(alignment: .top) {
+                Spacer()
+                
+                ZStack(alignment: .top) {
+                    Color.white
+                    
+                    VStack(spacing: 0) {
+                        
+                        Rectangle()
+                            .frame(height: safeAreaInsets.top)
+                            .foregroundColor(.white)
+                        
+                        self.content
+                    }
+                }
+                .frame(height: self.width)
+            }
+            .transition(.opacity.combined(with: .move(edge: .bottom)))
+            .offset(y: translation)
+            .gesture(
+                DragGesture()
+                    .updating($translation) { value, state, _ in
+                        if 0 <= value.translation.width {
+                            let translation = min(self.width, max(-self.width, value.translation.width))
+                            state = translation
+                        }
+                    }
+                    .onEnded({ value in
+                        if value.translation.width >= width/3 {
+                            self.isPresented = false
+                        }
+                    })
+            )
+        }
+        .edgesIgnoringSafeArea(.all)
+        // end ZStack
+    }
+}
+
